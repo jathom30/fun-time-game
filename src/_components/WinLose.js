343888f9318b236a97b7b83002c788c87ffeced6
Jeff Thomas <jeffry@eltoro.com>
2020-09-26 00:55:28 -0400

    cool

--- src/_components/WinLose.js
+++ src/_components/WinLose.js
@@ -1,10 +1,18 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { PositionContext } from '../PositionContext'
 
 export const WinLose = ({header, byline}) => {
   const { setReset } = useContext(PositionContext)
+  const [action, setAction] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAction(true)
+    },100)
+  },[])
+
   return (
-    <div className="WinLose">
+    <div className={`WinLose ${action ? 'blow-up' : ''}`}>
       <h1>{header}</h1>
       <p>{byline}</p>
       <button className="repeatBtn" onClick={() => setReset(true)}>Play again</button>
