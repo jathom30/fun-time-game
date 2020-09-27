d494efccdc8cc3d641400f1f077b8e0ea6878ab1
Jeff Thomas <jeffry@eltoro.com>
2020-09-27 14:25:40 -0400

    styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import image from '../img/dont_get_caught.PNG'
 import './home.scss'
 
 export const Home = () => {
   return (
     <div className="Home">
-      <div className="heroImg-Home">future image</div>
+      <div className="heroImg-Home"><img src={image} alt="logo"/></div>
       <h1 className="gameTitle-Home">Get Home Safe!</h1>
       <div className="routes-Home">
         <Link className="btn-routes" to="/board">Play</Link>
